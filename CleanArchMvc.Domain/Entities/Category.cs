cb23c28089318e4c86a10fa55326efe4d4d492c3
Arthur Pallu <arthurfelipepallu@gmail.com>
2022-02-11 18:21:35 -0300

    entities and category initial

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArchMvc.Domain.Validation;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get;private set; }
+        public ICollection<Product> Products {get;set;}
+
+
+        public Category(string name){
+            ValidateDomain(name);
+        }
+        public Category(int id,string name){
+            DomainExceptionValidation.When(id<0,"Invalid Id Value");
+            Id = i d;
+            ValidateDomain(name);
+        }
+
+        private void ValidateDomain(string name){
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),"Invalid name.Name is required");
+            DomainExceptionValidation.When(name.Length<3,"Invalid name, too short , minimum 3 characters");
+            DomainExceptionValidation.When(name.Length>50,"Invalid name, too long , maximum 50 characters");
+            Name = name;
+        }
+
+    }
+}
