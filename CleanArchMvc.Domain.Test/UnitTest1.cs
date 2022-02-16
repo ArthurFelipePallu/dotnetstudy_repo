cc3f1c089e66f5e42deaa10110cc5892cad56a44
Arthur Pallu <arthurfelipepallu@gmail.com>
2022-02-16 18:19:52 -0300

    xunit build sucesso

---
+++ CleanArchMvc.Domain.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+
+namespace CleanArchMvc.Domain.Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
