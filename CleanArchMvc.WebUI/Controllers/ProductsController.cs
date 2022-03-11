d751929c72f2409ad5f508a78bfd697ea752cdb5
Arthur Pallu <arthurfelipepallu@gmail.com>
2022-03-11 14:46:46 -0300

    Criando funcionalidades de product

--- CleanArchMvc.WebUI/Controllers/ProductsController.cs
+++ CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -30,6 +31,71 @@ namespace CleanArchMvc.WebUI.Controllers
             var products = await _productService.GetProducts();
             return View(products);
         }
+
+        [HttpGet()]
+        public IActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO productdto)
+        {
+            if(ModelState.IsValid)
+            {
+                await _productService.Add(productdto);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productdto);
+        }
+        // [HttpGet()]
+        // public async Task<IActionResult> Edit(int? id){
+        //     if(id == null) return NotFound();
+        //     var productdto = await _productService.GetById(id);
+        //     if(productdto == null) return NotFound();
+        //     return View(productdto);
+        // }
+        // [HttpPost()]
+        // public async Task<IActionResult> Edit(ProductDTO productdto)
+        // {
+        //     if(ModelState.IsValid)
+        //     {
+        //         try
+        //         {
+        //             await _productService.Update(productdto);
+        //         }
+        //         catch(Exception)
+        //         {
+        //             throw;
+        //         }
+        //         return RedirectToAction(nameof(Index));
+        //     }
+        //     return View(productdto);
+        // }
+
+        // [HttpGet()]
+        // public async Task<IActionResult> Delete(int? id) {
+        //     if(id==null)return NotFound();
+        //     var productdto = await _productService.GetById(id);
+        //     if(productdto==null) return NotFound();
+        //     return View(productdto);
+        // }
+        // //Não é possivel criar outra função com nome Delete e mesmos parametros 
+        // //precisa colocar  ActionName("Delete") para configurar o nome da action para Delete
+        // //Permite especificar um nome da action diferente do nome do metodo
+        // [HttpPost(),ActionName("Delete")]
+        // public async Task<IActionResult> DeleteConfirmed(int id) {
+        //     await _productService.Remove(id);
+        //     return RedirectToAction("Index");
+        // }
+
+        // [HttpGet()]
+        // public async Task<IActionResult> Details(int? id) {
+        //     if(id == null) return NotFound();
+        //     var productdto = await _productService.GetById(id);
+        //     if(productdto==null) return NotFound();
+        //     return View(productdto);
+        // }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
